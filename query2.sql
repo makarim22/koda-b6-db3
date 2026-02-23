d4603cc662495a65f7d661bf706349b09f8bc9c8
makarim muhammad <jakamakarim@gmail.com>
2026-02-23 14:06:17 +0700

    fix : add new query

--- query2.sql
+++ query2.sql
@@ -1,8 +1,11 @@
 
-select * from actors limit 10;
+select * from roles limit 10;
 
 -- query 1 : join directors dan genres ke tabel movies, limit 50 baris
 select md.director_id, d.first_name, d.last_name, m.name, m.year, m.rankscore, ms.genre from movies_directors md left join directors d on md.director_id = d.id left join movies m on md.movie_id = m.id inner join movies_genres ms on m.id=ms.movie_id limit 50; 
 
 -- query 2 : join movies dan roles berdasarkan table actors
 select * from roles r left join actors a on r.actor_id = a.id left join movies m on r.movie_id = m.id limit 50;
+
+---
+select * from actors a left join roles r on a.id = r.actor_id left join movies m on r.movie_id = m.id limit 10;
