866439d9d48f7af42dcf13f96859d247e86481ca
makarim muhammad <jakamakarim@gmail.com>
2026-02-23 10:57:01 +0700

    feat : create simple query

---
+++ query1.sql
@@ -0,0 +1,9 @@
+-- select * from movies;
+
+select * from movies where year > 2000;
+
+select * from actors where first_name ilike '%s' or last_name ilike '%s';
+
+select * from movies where rankscore between 5 and 7 and year between 2004 and 2006;
+
+select count(*) from movies where rankscore=6;
