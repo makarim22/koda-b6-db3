767cbe7c6c4080495988bb73237950e79893d204
makarim muhammad <jakamakarim@gmail.com>
2026-02-23 14:27:48 +0700

    fix : add aliasing to name in table movie

--- query2.sql
+++ query2.sql
@@ -4,7 +4,9 @@ select * from roles limit 10;
 -- query 1 : join directors dan genres ke tabel movies, limit 50 baris
 select md.director_id, d.first_name, d.last_name, m.name, m.year, m.rankscore, ms.genre from movies_directors md left join directors d on md.director_id = d.id left join movies m on md.movie_id = m.id inner join movies_genres ms on m.id=ms.movie_id limit 50; 
 
-select * from movies m 
+---
+select m.name, d.first_name, d.last_name, mg.genre
+ from movies m 
  left join movies_directors ms 
  on m.id = ms.movie_id 
  left join directors d 
@@ -17,7 +19,7 @@ select * from movies m
 select * from roles r left join actors a on r.actor_id = a.id left join movies m on r.movie_id = m.id limit 50;
 
 ---
-select *
+select a.*, r.role, m.name as movie_title, m.year as release_year
  from actors a
  left join roles r 
  on a.id = r.actor_id 
